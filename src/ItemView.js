6f16098e8c00415ed94ff466299669657717c449
rundun159 <xogud1231@gmail.com>
2021-05-18 02:54:07 +0900

    ItemView TH

--- src/ItemView.js
+++ src/ItemView.js
@@ -8,23 +8,70 @@ import {
   FlatList,
   SafeAreaView
 } from 'react-native';
-const Row = ({item}) =>{
+const Row_File = ({item}) =>{
+    console.log("Row_File");
+    console.log(item.file_path);
     return(
-    <View>
-        <Text>
-    { item.dir ? '📁' :'📄' }{'       '}{item.path}</Text>
+        <Text>  { '📄' }{'       '}{item.file_path}</Text>
+    );
+};
+const Row_Dir = ({item_list}) =>{
+    console.log("Row_Dir");
+    console.log(item_list.dir_path);
+    console.log(item_list.file_list);
+    const renderItem_Dir = ({item}) =>
+    {
+        console.log("renderItem");
+        console.log(item.file_path);
+        return(
+            <Text>  {'       '}{ '📄' }{'       '}{item.file_path}</Text>
+        );
+    }
+    return(
+        <SafeAreaView>
+        <Text>  { '📁' }{'       '}{item_list.dir_path}</Text>
+        <FlatList
+            data = {item_list.file_list}
+            renderItem = {renderItem_Dir}
+        />
+        </SafeAreaView>
+    );
+};
+const Row_File_Dir = ({item}) =>{
+    console.log('Row_FIle_Dir');
+    console.log(item.dir_path);
+    console.log(item.dir);
+    return(
+        <View>
+    { item.dir && 
+        <Row_Dir 
+            item_list = {item}/>
+    }
+    { !item.dir && 
+        <Row_File
+            item = {item} />
+    }
     </View>
     );
 };
 const ItemView = ({items, /* curDir, setCurDir, */ checkedItems, setCheckedItems }) => {
     const [checkAll, setCheckAll] = useState(false);
-    const [itemList, addItem] = useState([{path:'/home/th/memo.txt',bool:true,dir:false}]);
+    const [itemList, addItem] = useState([{dir_path : '', file_path:'/home/th/memo.txt',file_list:[],bool:true,dir:false},
+    {
+        dir_path : "home/th/",
+        file_path : '',
+        file_list : [{file_path : '/home/th/memo1.txt'},{file_path : '/home/th/memo2.txt'}],
+        bool : true,
+        dir : true
+    }
+    ]);
 
     const renderItem=({item})=>{
-        return(<Row
+        return(<Row_File_Dir
             item = {item}
         />);   
     };
+    console.log("Item View Return");
     return(
         <SafeAreaView style = {styles.itemview}>
             <FlatList
